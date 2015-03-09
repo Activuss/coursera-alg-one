5aacffa93ef8d372fade3a7287a0471918d5d0e7
illya.kolykhanin <illya.kolykhanin@sigmaukraine.com>
2015-03-09 13:38:06 +0200

    added methods implementation stub

---
+++ Week2Queues/src/Subset.java
@@ -0,0 +1,8 @@
+public class Subset {
+    public static void main(String[] args) {
+        /*Write a client program Subset.java that takes a command-line integer k; reads in a sequence of N strings from
+        standard input using StdIn.readString(); and prints out exactly k of them, uniformly at random. Each item from
+        the sequence can be printed out at most once. You may assume that 0 ≤ k ≤ N, where N is the number of string
+        on standard input.*/
+    }
+}
