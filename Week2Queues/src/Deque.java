360c5c664f24e8fe3cc3098dc48da72f1e1edd5a
illya.kolykhanin <illya.kolykhanin@sigmaukraine.com>
2015-03-09 18:38:29 +0200

    deque linked list implementation

--- Week2Queues/src/Deque.java
+++ Week2Queues/src/Deque.java
@@ -2,50 +2,101 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-    public Deque() {                           // construct an empty deque
+    private int N;          // size of the stack
+    private Node pre;       // sentinel before first item
+    private Node post;     // sentinel after last item
 
+    // helper linked list class
+    private class Node {
+        private Item item;
+        private Node next;
+        private Node prev;
+    }
+
+    public Deque() {                           // construct an empty deque
+        pre = new Node();
+        post = new Node();
+        pre.next = post;
+        post.prev = pre;
+        N = 0;
     }
     public boolean isEmpty() {                // is the deque empty?
-        return false;
+        return N == 0;
     }
     public int size() {                         // return the number of items on the deque
-        return 0;
+        return N;
     }
     public void addFirst(Item item) {         // add the item to the front
         if (item == null) {
             throw new NullPointerException();
         } else {
-
+            Node first = pre.next;
+            Node x = new Node();
+            x.item = item;
+            x.prev = pre;
+            x.next = first;
+            pre.next = x;
+            first.prev = x;
+            N++;
         }
     }
     public void addLast(Item item) {          // add the item to the end
         if (item == null) {
             throw new NullPointerException();
         } else {
-
+            Node last = post.prev;
+            Node x = new Node();
+            x.item = item;
+            x.next = post;
+            x.prev = last;
+            post.prev = x;
+            last.next = x;
+            N++;
         }
     }
     public Item removeFirst() {               // remove and return the item from the front
-        return null;
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = pre.next.item;
+        Node newPre = new Node();
+        newPre.next = pre.next.next;
+        pre.next.next.prev = newPre;
+        pre = null;
+        pre = newPre;
+        N--;
+
+        return item;
     }
     public Item removeLast() {                // remove and return the item from the end
-        return null;
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = post.prev.item;
+        Node newPost = new Node();
+        newPost.prev = post.prev.prev;
+        post.prev.prev.next = newPost;
+        post = null;
+        post = newPost;
+        N--;
+
+        return item;
     }
+
     public Iterator<Item> iterator() {        // return an iterator over items in order from front to end
         Iterator iterator = new Iterator() {
+            private Node current = pre.next;
+            int index = 0;
             @Override
             public boolean hasNext() {
-                return false;
+                return index < N;
             }
 
             @Override
-            public Object next() {
+            public Item next() {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
-                } else {
-
                 }
-                return null;
+                Item item = current.item;
+                current = current.next;
+                index++;
+                return item;
             }
 
             @Override
@@ -56,8 +107,45 @@ public class Deque<Item> implements Iterable<Item> {
         return iterator;
     }
 
-    public static void main(String[] args) {   // unit testing
-
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        Node node = pre.next;
+        for (int i = 0; i < N; i++) {
+            s.append(node.item);
+            s.append(" ");
+            node = node.next;
+        }
+        return s.toString();
     }
 
+
+    public static void main(String[] args) {   // unit testing
+    Deque<Integer> integers = new Deque<Integer>();
+        System.out.println(integers);
+        integers.addFirst(1);
+        System.out.println(integers);
+        integers.addFirst(2);
+        System.out.println(integers);
+        integers.addFirst(3);
+        System.out.println(integers);
+        integers.addLast(4);
+        System.out.println(integers);
+        integers.addLast(5);
+        System.out.println(integers);
+        System.out.println("             ");
+
+        for (Integer items : integers){
+            System.out.println(items);
+        }
+
+        System.out.println("removing first: " + integers.removeFirst());
+        System.out.println(integers);
+        System.out.println("removing last: " + integers.removeLast());
+        System.out.println(integers);
+        integers.removeLast();
+        System.out.println(integers);
+        integers.removeLast();
+        integers.removeLast();
+        System.out.println(integers);
+    }
 }
