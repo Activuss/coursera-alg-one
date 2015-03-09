346dfe0bf0051e3db74d73094051db061a27217d
illya.kolykhanin <illya.kolykhanin@sigmaukraine.com>
2015-03-09 13:34:34 +0200

    added methods implementation stub

--- Week2Queues/src/Deque.java
+++ Week2Queues/src/Deque.java
@@ -1,2 +1,63 @@
-public class Deque {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+    public Deque() {                           // construct an empty deque
+
+    }
+    public boolean isEmpty() {                // is the deque empty?
+        return false;
+    }
+    public int size() {                         // return the number of items on the deque
+        return 0;
+    }
+    public void addFirst(Item item) {         // add the item to the front
+        if (item == null) {
+            throw new NullPointerException();
+        } else {
+
+        }
+    }
+    public void addLast(Item item) {          // add the item to the end
+        if (item == null) {
+            throw new NullPointerException();
+        } else {
+
+        }
+    }
+    public Item removeFirst() {               // remove and return the item from the front
+        return null;
+    }
+    public Item removeLast() {                // remove and return the item from the end
+        return null;
+    }
+    public Iterator<Item> iterator() {        // return an iterator over items in order from front to end
+        Iterator iterator = new Iterator() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public Object next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                } else {
+
+                }
+                return null;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return iterator;
+    }
+
+    public static void main(String[] args) {   // unit testing
+
+    }
+
 }
--- Week2Queues/src/RandomizedQueue.java
+++ Week2Queues/src/RandomizedQueue.java
@@ -1,2 +1,65 @@
-public class RandomizedQueue {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    public RandomizedQueue() {                // construct an empty randomized queue
+
+    }
+    public boolean isEmpty() {                // is the queue empty?
+        return false;
+    }
+    public int size() {                        // return the number of items on the queue
+        return 0;
+    }
+    public void enqueue(Item item) {          // add the item
+        if (item == null) {
+            throw new NullPointerException();
+        } else {
+
+        }
+
+    }
+    public Item dequeue() {                    // remove and return a random item
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        } else {
+
+        }
+        return null;
+    }
+    public Item sample() {                     // return (but do not remove) a random item
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        } else {
+
+        }
+        return null;
+    }
+    public Iterator<Item> iterator() {        // return an independent iterator over items in random order
+        Iterator iterator = new Iterator() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public Object next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                } else {
+
+                }
+                return null;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return iterator;
+    }
+    public static void main(String[] args) {   // unit testing
+        
+    }
 }
